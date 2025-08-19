74af0db9db58e591673f4aab25d7fa0696f1e049
joaquindecena40@gmail.com <joaquinraphaelvaldezdecena@Joshuas-MacBook-Pro.local>
2025-08-19 22:47:47 +0800

    added: course info

---
+++ part1/course-info/src/Header.jsx
@@ -0,0 +1,15 @@
+function Header(){
+    return (
+      // <header className="flex items-center justify-center bg-slate-900">
+      //     <h1 className="text-4xl font-bold text-blue-400">
+      //         Hello World
+      //     </h1>
+      // </header>
+
+      <div className="p-6">
+        <button className="btn btn-primary">Hello daisyUI</button>
+      </div>
+    );
+}
+
+export default Header;
